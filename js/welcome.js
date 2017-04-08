f58a6e672da60107bf4301d6f82a86d36b78a522
talia-walkey <twrox8@gmail.com>
2017-04-08 11:58:19 -0700

    Login and Home Done

---
+++ js/welcome.js
@@ -0,0 +1,20 @@
+var lbackground = document.getElementById("lbackground");
+    lbackground.src = "bg-1.svg";
+
+var lwelcome = document.getElementById("lwelcome");
+    lwelcome.src = "hwelcome.svg";
+
+var lx = document.getElementById("lx");
+    lx.src = "hx.svg";
+
+var llogin = document.getElementById("llogin");
+    llogin.src = "login-btn.svg";
+
+var lsignup = document.getElementById("lsignup");
+    lsignup.src = "hsignup.svg";
+
+var lguest = document.getElementById("lguest");
+    lguest.src = "hguest.svg";
+
+var lcancel = document.getElementById("lcancel");
+    lcancel.src = "cancel.svg";
