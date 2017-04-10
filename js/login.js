f2b479b7c836564b62982e48b97bc75b3a93f353
talia-walkey <twrox8@gmail.com>
2017-04-09 21:32:35 -0700

    Media Queries are really hard
    I cannot get the background to work! Everything else is fine.

---
+++ js/login.js
@@ -0,0 +1,18 @@
+var lback = document.getElementById("lback");
+    lback.src = "back.svg";
+
+var lbackground = document.getElementById("lbackground");
+    lbackground.src = "bg-1.svg";
+
+var lok = document.getElementById("lok");
+    lok.src = "ok.svg";
+
+var lcancel = document.getElementById("lcancel");
+    lcancel.src = "cancel.svg";
+
+var LUN = document.getElementById("LUN");
+    LUN.src = "input.svg";
+
+var LPW = document.getElementById("LPW");
+    LPW.src = "input.svg";
+
