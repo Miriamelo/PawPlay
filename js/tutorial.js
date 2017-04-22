dbcbef8b3007dba29af38968a13bd861c23c32a3
talia-walkey <twrox8@gmail.com>
2017-04-22 16:42:59 -0700

    Fixed tutorial

--- js/tutorial.js
+++ js/tutorial.js
@@ -1,3 +1,5 @@
+var mq =window.matchMedia("(min-width: 500px)");
+
             var tutnext1 = document.getElementById("tutnext1");
             var tutnext2 = document.getElementById("tutnext2");
             var tutnext3 = document.getElementById("tutnext3");
@@ -6,13 +8,15 @@
             var ttut1 = document.getElementById("ttut1");
             var ttut2 = document.getElementById("ttut2");
             var ttut3 = document.getElementById("ttut3");
-            
-            
+
+
+if(mq.matches){
             tutnext1.addEventListener("click", function(){
                 ttut2.style.display= "inline-block";
                 ttut1.style.display= "none";
                 tutnext1.style.display = "none";
                 tutnext2.style.display = "inline-block";
+                
             });
 
             tutnext2.addEventListener("click", function(){
@@ -28,4 +32,12 @@
                 tutnext3.style.display = "none";
                 tutnext4.style.display = "inline-block";
             });
+}
 
+
+else{
+    
+    
+    
+    
+}
