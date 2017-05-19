8baf6aa973ea67b27ceb174629ea7e7f00a21f04
talia-walkey <twrox8@gmail.com>
2017-05-18 21:34:38 -0700

    Made Mobile tutorial and fixed layout for pages

--- js/tutorial.js
+++ js/tutorial.js
@@ -1,5 +1,5 @@
-var mq =window.matchMedia("(min-width: 500px)");
 
+    console.log("under 500");
             var tutnext1 = document.getElementById("tutnext1");
             var tutnext2 = document.getElementById("tutnext2");
             var tutnext3 = document.getElementById("tutnext3");
@@ -9,22 +9,20 @@ var mq =window.matchMedia("(min-width: 500px)");
 
 
             var ttut1 = document.getElementById("ttut1");
+                    ttut1.src = "./img/FinalTutorial1Mobile.svg";
             var ttut2 = document.getElementById("ttut2");
+                    ttut2.src = "./img/FinalTutorial2Mobile.svg";
             var ttut3 = document.getElementById("ttut3");
+                    ttut3.src = "./img/FinalTutorial3Mobile.svg";
             var ttut4 = document.getElementById("ttut4");
+                    ttut4.src = "./img/FinalTutorial4Mobile.svg";
             var ttut5 = document.getElementById("ttut5");
+                    ttut5.src = "./img/FinalTutorial5Mobile.svg";
             var ttut6 = document.getElementById("ttut6");
+                    ttut6.src = "./img/FinalTutorial6Mobile.svg";
 
+           
 
-            var ttut11 = document.getElementById("ttut11");
-            var ttut21 = document.getElementById("ttut21");
-            var ttut31 = document.getElementById("ttut31");
-            var ttut41 = document.getElementById("ttut41");
-            var ttut51 = document.getElementById("ttut51");
-            var ttut61 = document.getElementById("ttut61");
-
-
-if(mq.matches){
             tutnext1.addEventListener("click", function(){
                 ttut2.style.display= "inline-block";
                 ttut1.style.display= "none";
@@ -59,6 +57,70 @@ if(mq.matches){
                 tutnext5.style.display = "none";
                 tutnext6.style.display = "inline-block";
             }); 
+
+
+
+var mq = window.matchMedia( "(min-width: 500px)" );
+
+
+if (mq.matches) {
+     console.log("over 500");
+            var tutnext1 = document.getElementById("tutnext1");
+            var tutnext2 = document.getElementById("tutnext2");
+            var tutnext3 = document.getElementById("tutnext3");
+            var tutnext4 = document.getElementById("tutnext4");
+            var tutnext5 = document.getElementById("tutnext5");
+            var tutnext6 = document.getElementById("tutnext6");
+
+
+            var ttut1 = document.getElementById("ttut1");
+                    ttut1.src = "./img/FinalTutorial1.svg";
+            var ttut2 = document.getElementById("ttut2");
+                    ttut2.src = "./img/FinalTutorial2.svg";
+            var ttut3 = document.getElementById("ttut3");
+                    ttut3.src = "./img/FinalTutorial3.svg";
+            var ttut4 = document.getElementById("ttut4");
+                    ttut4.src = "./img/FinalTutorial4.svg";
+            var ttut5 = document.getElementById("ttut5");
+                    ttut5.src = "./img/FinalTutorial5.svg";
+            var ttut6 = document.getElementById("ttut6");
+                    ttut6.src = "./img/FinalTutorial6.svg";
+
+        
+
+            tutnext1.addEventListener("click", function(){
+                ttut2.style.display= "inline-block";
+                ttut1.style.display= "none";
+                tutnext1.style.display = "none";
+                tutnext2.style.display = "inline-block";
+            });
+
+            tutnext2.addEventListener("click", function(){
+                ttut3.style.display= "inline-block";
+                ttut2.style.display= "none";
+                tutnext2.style.display = "none";
+                tutnext3.style.display = "inline-block";
+            });
+
+            tutnext3.addEventListener("click", function(){
+                ttut4.style.display= "inline-block";
+                ttut3.style.display= "none";
+                tutnext3.style.display = "none";
+                tutnext4.style.display = "inline-block";
+            });
     
-}
+            tutnext4.addEventListener("click", function(){
+                ttut5.style.display= "inline-block";
+                ttut4.style.display= "none";
+                tutnext4.style.display = "none";
+                tutnext5.style.display = "inline-block";
+            });
+    
+            tutnext5.addEventListener("click", function(){
+                ttut6.style.display= "inline-block";
+                ttut5.style.display= "none";
+                tutnext5.style.display = "none";
+                tutnext6.style.display = "inline-block";
+            }); 
+};
 
